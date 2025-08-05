b374270683841193c770ed40bed4b8da6d01e968
ДНС <alym0v.egor@yandex.ru>
2025-08-05 14:59:09 +0300

    dom4.2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,67 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 1");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 2");
+        for (int i = 0; i < (arr.length - 1); i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println(arr[arr.length - 1]);
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
+    }
+
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        System.out.println("Задача 3");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        double sred = sum / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + sred + " рублей");
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
